862d6619c3b5cc79b0e2e435c24e907e723e1b2b
MKBookwala <166864796+MKBookwala@users.noreply.github.com>
2024-06-10 01:23:12 +0530

    Update TELECOM_CHURN - CASE STUDY - MURTAZA BOOKWALA.py

--- TELECOM_CHURN - CASE STUDY - MURTAZA BOOKWALA.py
+++ TELECOM_CHURN - CASE STUDY - MURTAZA BOOKWALA.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # ### Problem statement:-
 # To reduce customer churn, telecom companies need to predict which customers are at high risk of churn.
 # In this project, we will analyse customer-level data of a leading telecom firm, build predictive models to identify customers at high risk of churn and identify the main indicators of churn.
